269781acb439f6544b9f559fbef6c217b23fb472
Yann ANTONINI <darjador280@gmail.com>
2020-03-13 10:58:52 +0100

    test 2 : vérification de l'accès à la route 'aircraft'

--- projet_TDD/APILS/tests.py
+++ projet_TDD/APILS/tests.py
@@ -2,14 +2,21 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from APILS.models import *
 from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate, APIClient
+from requests.auth import HTTPBasicAuth
+from rest_framework.test import RequestsClient
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+import simplejson as json
+from APILS import views
+
 
 # Create your tests here.
 class ListAPIViewTestCase(APITestCase):
 
     def setUp(self):
         #create test user
-        test_user = User.objects.create_user('testuser', 'testemail@test.com', 'P@ssw0rd')
-        test_user.save()
+        self.user = User.objects.create_user(username="testuser", email="user1@test.com", password="password1", is_staff=True)
+        self.client = APIClient()
 
         #create fake models
         test_airframer = Airframer.objects.create(name='test_airframer')
@@ -17,12 +24,47 @@ class ListAPIViewTestCase(APITestCase):
         test_aircraft = Aircraft.objects.create(airframer=Airframer.objects.get(name=test_airframer.name), name='test_aircraft')
         test_aircraft.save()
 
+        test_productline = ProductLine.objects.create(name='test_productline')
+        test_productline.save()
+
+        test_systemname = SystemName.objects.create(name='test_systemname')
+        test_systemname.save()
+
+        test_lru_denom = LruDenomination.objects.create(name='test_lru_denom')
+        test_lru_denom.save()
+
+        test_sru_denom = SruDenomination.objects.create(name='test_sru_denom')
+        test_sru_denom.save()
+
+        test_lru = LRU.objects.create(
+            product_line=ProductLine.objects.get(name=test_productline.name),
+            system_name=SystemName.objects.get(name=test_systemname.name),
+            aircraft=Aircraft.objects.get(name=test_aircraft.name),
+            lru_denomination=LruDenomination.objects.get(name=test_lru_denom.name),
+            pn='test_lru')
+        test_lru.save()
+
+        test_sru = SRU.objects.create(
+            sru_denomination=SruDenomination.objects.get(name=test_sru_denom.name),
+            lru=LRU.objects.get(pn=test_lru.pn),
+            pn='test_sru')
+        test_sru.save()
+
     def test_access_api_without_credentials(self):
-
         #creation of the request
-        response = self.client.post('http://localhost:8000/')
-
-        #print(response)
+        response = self.client.get('http://localhost:8000/users/')
 
         #response verification
         self.assertEqual(response.data, {"detail": "Authentication credentials were not provided."})
+
+    def test_get_aircraft_from_api(self):
+        #authentication
+        # Include an appropriate `Authorization:` header on all requests.
+        client = APIClient()
+
+        #creation of the request
+        response = client.post('http://localhost:8000/getAircraft/', {'aircraft': 'test_aircraft'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        #response verification
+        # self.assertEqual(response.data, {"aircraft": "aircraft1", "flight_hours_per_year": "null", "Warrranty_duration": "null"})
--- projet_TDD/APILS/views.py
+++ projet_TDD/APILS/views.py
@@ -6,6 +6,11 @@ from APILS.models import *
 from projet_TDD.serializers import *
 from django.contrib.auth.models import User
 from projet_TDD.serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
+import simplejson as json  
+
 
 # Create your views here.
 
@@ -16,3 +21,33 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def get_aircraft(request):
+    """API endpoint that return datas viewed in he dashboard
+    according to parameters in POST request
+
+    Arguments:
+        request {[type]} -- [description]
+
+    Returns:
+        [HttpResponse] -- [JSON]
+    """
+
+    try:
+        current = {}
+        aircraft = Aircraft.objects.get(name=request.data['aircraft']) 
+
+        current = {
+            'aircraft': aircraft.name,
+            'flight_hours_per_year': aircraft.fh_year,
+            'Warrranty_duration': aircraft.warranty_duration_new_product,
+            
+        }
+
+        return HttpResponse(json.dumps(current), status=200)
+    except Exception as identifier:
+        print("A problem occured :"+str(identifier))
+        return HttpResponseNotFound('<h1>FATAL ERROR </h1>\n' + str(identifier))
